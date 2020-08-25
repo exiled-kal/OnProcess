9dc9d0de795cdd268c00d75f36ac362bdcff4429
kaldne <tseringkalden1964@gmail.com>
2020-08-25 13:22:11 -0700

    new changes

---
+++ .history/photo/src/component/header/Header_20200825124854.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="headerContainer">
+      <img
+        className="header__photo"
+        src="https://previews.dropbox.com/p/thumb/AA4lBNOM7W6MsPmBqs9_xpC9xkxiBiuUBb4Th0Wr5YcvkOa9Be-v-oek0mD-7Jf4WnvtWgyZwSyXDmY5MkOHUTOFgrubE4GJVi7EVh1NSAaJwElcnxxVME6g-RkVYeGN_45k1EeauBFyyHLeWRa6G6MWhfBSbYsq7TYGqRnl-1kfQF3xhNAP3hCcCsVi5_VDhBHUaYekBehbNWD__wkwl30wopvng3YoWF4K3t2BJJxxwqn2CNMgHTuMXroC0XuvoGB2JWEo-0qoOMj-xPeEsnHm3vu5E29843csWtaSyQRfNbYaV1r51ur4FvNcNfmPKAIBB-PUKhdFfb39idSSf3QE/p.jpeg?fv_content=true&size_mode=5"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Header;
